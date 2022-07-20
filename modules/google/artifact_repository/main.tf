40ddf1218b844667c8dac07a7c11db1de5f69edb
Rick Morrow <rick.morrow1204@gmail.com>
2022-07-20 09:13:48 -0500

    Add artifact repository Terraform module (#2)
    * Add artifact repository Terraform module
    
    * make description more accurate
    
    * Change default region to be us-south1

---
+++ modules/google/artifact_repository/main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project = var.project_id
+  region  = var.location
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.location
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  provider = google-beta
+
+  location      = var.location
+  repository_id = var.repository_id
+  description   = "Terraform generated docker repository."
+  format        = var.format
+}
---
+++ modules/google/artifact_repository/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  type        = string
+  description = "Id for the Google Cloud project."
+}
+
+variable "repository_id" {
+  type        = string
+  description = "Name for the generated repo."
+}
+
+variable "location" {
+  type        = string
+  default     = "us-south1"
+  description = "Location for the Artifact repo."
+}
+
+variable "format" {
+  type        = string
+  default     = "docker"
+  description = "Which type of artifact repo to create. (ex: docker)."
+}
